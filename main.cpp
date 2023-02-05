f1e744de10d4e42ccdec2aa7a6fa951afd1a2f95
akim-nas <98978747+akim-nas@users.noreply.github.com>
2023-02-04 21:30:38 -0500

    added gitignore and started base prompt

--- main.cpp
+++ main.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
+#include <limits>
 
 int main() {
-    std::cout << "hello world";
+    int log_or_create;
+
+    std::cout << "The Intergalactic Space Bank of Santa Fe\n\n" /* prompt */
+              << "[1] Create Account\n"                         /* prompt */
+              << "[2] Login\n";                                 /* prompt */
+    
+    std::cin  >> log_or_create; /* user login/create response */
+
+    /* prevent unwanted input */
+    while (std::cin.fail() || (log_or_create < 1 || log_or_create > 2)) {
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+        std::cout << "Invalid input, please input a number listed above: ";
+        
+        std::cin >> log_or_create;
+    }
+
+    switch (log_or_create) {
+    case 1: /* create account */
+        std::cout << "case 1";
+        break;
+
+    case 2: /* login account */
+        std::cout << "case 2";
+        break;
+    }
+
     return 0;
 }
